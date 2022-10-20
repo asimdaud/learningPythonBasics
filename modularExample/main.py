018d50d0290dd5d50aeaf3f3cb9c657ae2d50c73
Asim <asim.scorpio9@gmail.com>
2022-10-20 12:04:38 +0100

    importing test

---
+++ modularExample/main.py
@@ -0,0 +1,8 @@
+# Adds and multiplies the integers provided.
+# To run --> pyhton main.py
+
+
+from pythonAddFunctionTest import *
+
+
+start(user_input="")
--- modularExample/pythonAddFunctionTest.py
+++ modularExample/pythonAddFunctionTest.py
@@ -1,8 +1,4 @@
-# Adds and multiplies the integers provided.
-# To run --> pyhton pythonAddFunctionTest.py
-
-
-def adding(a, b):
+def adding(a, b, user_input):
     try:
         a = int(a)
         return a + b
@@ -13,7 +9,7 @@ def adding(a, b):
             print("Terminating the program!")
 
 
-def multiply(a, b):
+def multiply(a, b, user_input):
     try:
         a = int(a)
         return a * b
@@ -24,9 +20,9 @@ def multiply(a, b):
             print("Terminating the program!")
 
 
-def start():
+def start(user_input):
     try:
-        user_input = ""
+        # user_input = ""
         initial_choice = int(input("Enter 1 for addition and 2 for multiplication: \n"))
         while (
             user_input != "exit" and user_input != "Exit" and user_input != "EXIT"
@@ -35,7 +31,7 @@ def start():
                 user_input = input("Enter numbers to add separating with comma (,): \n")
                 var_add = 0
                 for number_element in user_input.split(","):
-                    var_add = adding(number_element, var_add)
+                    var_add = adding(number_element, var_add, user_input)
                 print("The answer is: ", var_add)
             elif int(initial_choice) == 2:
                 user_input = input(
@@ -43,11 +39,8 @@ def start():
                 )
                 var_multiply = 1
                 for number_element in user_input.split(","):
-                    var_multiply = multiply(number_element, var_multiply)
+                    var_multiply = multiply(number_element, var_multiply, user_input)
                 print("The answer is: ", var_multiply)
     except ValueError:
         print("Invalid choice, terminating program....")
 
-
-
-start()
