28c61e01b425115fc0de6a9ab0b06646ff6fd62a
Asim <45770583+asimdaud@users.noreply.github.com>
2022-10-20 16:23:05 +0100

    Update teslaStockData.py

--- teslaStockData.py
+++ teslaStockData.py
@@ -1,3 +1,6 @@
+# Outputs the tesla stock data 
+# Tesla, Inc. :  213.73
+
 import urllib.request, json
 resp = urllib.request.urlopen('https://query2.finance.yahoo.com/v10/finance/quoteSummary/tsla?modules=price')
 data = json.loads(resp.read())
